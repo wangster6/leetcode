a3fe6eb4c3dc05b72a1f11d56cdd13716452d216
Ray Wang <raywang612@gmail.com>
2025-01-18 10:31:55 -0500

    P13 Python Attempt 1 (Assisted)

---
+++ 13/python/attempt1.py
@@ -0,0 +1,24 @@
+# COMPLETED AFTER VIEWING SOLUTION
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        number = 0
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        
+        return number
